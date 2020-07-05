bdea2941af856687bbd5d13560b032acd474634d
rpavon <rpavon@leverade.com>
2020-07-05 15:16:05 +0200

    Refactored tests to add before function

--- app/src/test/java/com/lookingclowns/connect3/tests/domain/valueobjects/gameboard/GameBoardUnitTest.java
+++ app/src/test/java/com/lookingclowns/connect3/tests/domain/valueobjects/gameboard/GameBoardUnitTest.java
@@ -2,15 +2,23 @@ package com.lookingclowns.connect3.tests.domain.valueobjects.gameboard;
 
 import com.lookingclowns.connect3.domain.valueobjects.gameboard.GameBoard;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public final class GameBoardUnitTest {
 
+    private GameBoard gameBoard;
+
+    @Before
+    public void setUp() {
+        this.gameBoard = new GameBoard();
+    }
+
     @Test
     public void it_should_create_create_token_in_correct_place_of_array() {
-        GameBoard gameBoard = new GameBoard();
+
         int position = 3;
         int playerId = 1;
         gameBoard.addToken(position, playerId);
