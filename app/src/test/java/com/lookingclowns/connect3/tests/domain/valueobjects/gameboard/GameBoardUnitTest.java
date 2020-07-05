6b1ba640a20ad6cb7c2a3adc806bc8550da7b9cf
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-05 17:27:19 +0200

    Added test to check that an exception is thrown if position is already taken by the active player

--- app/src/test/java/com/lookingclowns/connect3/tests/domain/valueobjects/gameboard/GameBoardUnitTest.java
+++ app/src/test/java/com/lookingclowns/connect3/tests/domain/valueobjects/gameboard/GameBoardUnitTest.java
@@ -1,6 +1,7 @@
 package com.lookingclowns.connect3.tests.domain.valueobjects.gameboard;
 
 import com.lookingclowns.connect3.domain.exceptions.playerisnotallowed.PlayerIsNotAllowedException;
+import com.lookingclowns.connect3.domain.exceptions.positionalreadyselectedbyplayer.PositionAlreadySelectedByPlayerException;
 import com.lookingclowns.connect3.domain.exceptions.positionistaken.PositionIsTakenException;
 import com.lookingclowns.connect3.domain.valueobjects.gameboard.GameBoard;
 
@@ -19,7 +20,10 @@ public final class GameBoardUnitTest {
     }
 
     @Test
-    public void it_should_create_create_token_in_correct_place_of_array() throws PlayerIsNotAllowedException, PositionIsTakenException {
+    public void it_should_create_create_token_in_correct_place_of_array()
+            throws PlayerIsNotAllowedException,
+            PositionIsTakenException,
+            PositionAlreadySelectedByPlayerException {
         int position = 3;
         int playerId = 1;
         gameBoard.addToken(position, playerId);
@@ -30,18 +34,35 @@ public final class GameBoardUnitTest {
     }
 
     @Test(expected = PlayerIsNotAllowedException.class)
-    public void it_should_throw_an_exception_if_player_is_not_allowed() throws PlayerIsNotAllowedException, PositionIsTakenException {
+    public void it_should_throw_an_exception_if_player_is_not_allowed()
+            throws PlayerIsNotAllowedException,
+            PositionIsTakenException,
+            PositionAlreadySelectedByPlayerException {
         int position = 3;
         int playerId = 2;
         gameBoard.addToken(position, playerId);
     }
 
     @Test(expected = PositionIsTakenException.class)
-    public void it_should_throw_an_exception_if_position_is_taken() throws PositionIsTakenException, PlayerIsNotAllowedException {
+    public void it_should_throw_an_exception_if_position_is_taken()
+            throws PositionIsTakenException,
+            PlayerIsNotAllowedException,
+            PositionAlreadySelectedByPlayerException {
         int position = 3;
         int playerId = 0;
         int anotherPlayerId = 1;
         gameBoard.addToken(position, playerId);
         gameBoard.addToken(position, anotherPlayerId);
+    }
+
+    @Test(expected = PositionAlreadySelectedByPlayerException.class)
+    public void it_should_throw_an_exception_if_active_player_is_trying_to_reselect_a_position()
+            throws PlayerIsNotAllowedException,
+            PositionIsTakenException,
+            PositionAlreadySelectedByPlayerException {
+        int position = 3;
+        int playerId = 0;
+        gameBoard.addToken(position, playerId);
+        gameBoard.addToken(position, playerId);
     }
 }
