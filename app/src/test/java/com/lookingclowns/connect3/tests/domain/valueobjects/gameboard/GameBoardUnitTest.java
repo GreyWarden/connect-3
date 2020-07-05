9ea58694c25660ffd471d13c2fc6a8470341a1a1
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-05 15:34:32 +0200

    Added test to check that an exception is thrown if position is already taken

--- app/src/test/java/com/lookingclowns/connect3/tests/domain/valueobjects/gameboard/GameBoardUnitTest.java
+++ app/src/test/java/com/lookingclowns/connect3/tests/domain/valueobjects/gameboard/GameBoardUnitTest.java
@@ -1,6 +1,7 @@
 package com.lookingclowns.connect3.tests.domain.valueobjects.gameboard;
 
 import com.lookingclowns.connect3.domain.exceptions.playerisnotallowed.PlayerIsNotAllowedException;
+import com.lookingclowns.connect3.domain.exceptions.positionistaken.PositionIsTakenException;
 import com.lookingclowns.connect3.domain.valueobjects.gameboard.GameBoard;
 
 import org.junit.Before;
@@ -18,7 +19,7 @@ public final class GameBoardUnitTest {
     }
 
     @Test
-    public void it_should_create_create_token_in_correct_place_of_array() throws PlayerIsNotAllowedException {
+    public void it_should_create_create_token_in_correct_place_of_array() throws PlayerIsNotAllowedException, PositionIsTakenException {
         int position = 3;
         int playerId = 1;
         gameBoard.addToken(position, playerId);
@@ -29,9 +30,18 @@ public final class GameBoardUnitTest {
     }
 
     @Test(expected = PlayerIsNotAllowedException.class)
-    public void it_should_throw_an_exception_if_player_is_not_allowed() throws PlayerIsNotAllowedException {
+    public void it_should_throw_an_exception_if_player_is_not_allowed() throws PlayerIsNotAllowedException, PositionIsTakenException {
         int position = 3;
         int playerId = 2;
         gameBoard.addToken(position, playerId);
+    }
+
+    @Test(expected = PositionIsTakenException.class)
+    public void it_should_throw_an_exception_if_position_is_taken() throws PositionIsTakenException, PlayerIsNotAllowedException {
+        int position = 3;
+        int playerId = 0;
+        int anotherPlayerId = 1;
+        gameBoard.addToken(position, playerId);
+        gameBoard.addToken(position, anotherPlayerId);
     }
 }
